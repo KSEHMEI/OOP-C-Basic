e89014d55006da69f2974f8ef296c2ec08887788
Mikael Nias <mikael17125@gmail.com>
2020-07-07 19:26:03 +0700

    add module 5 test

---
+++ 5/Multi.cpp
@@ -0,0 +1,16 @@
+#include "Multi.h"
+
+int main()
+{
+    
+    num = 5;
+    denum = 10;
+
+    std::cout<<calc(5,10)<<std::endl;
+
+    return 0;
+}
+
+double calc(int num, int denum){
+    return (double)num/denum;
+}
---
+++ 5/Multi.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int num;
+int denum;
+
+double calc(int num, int denum);
